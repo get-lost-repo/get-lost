a73a1ca39afdf6a93ac64e7b9ae873f2d80543ba
Jon Gentry <jonmarkgentry@gmail.com>
2019-01-31 12:17:09 -0800

    comments functioning

--- js/belfair.js
+++ js/belfair.js
@@ -7,13 +7,14 @@ var commentList = document.getElementById('comment-list');
 var commentForm = document.getElementById('comment-form');
 var belfairComments = [];
 
-var dataBack = JSON.parse(localStorage.getItem('belfairComments'));
+
 // ****************************************************
 // Constructor for constructing the comments
 // ****************************************************
 var Comment = function(userName, text) {
   this.userName = userName;
   this.text = text;
+  belfairComments.unshift(this);
 };
 
 Comment.prototype.render = function() {
@@ -22,22 +23,31 @@ Comment.prototype.render = function() {
   return liEl;
 };
 
-// *****************************************************
-// FUNCTION DECLARATIONS
-// *****************************************************
+var dataBack = JSON.parse(localStorage.getItem('belfairCommentsStringified'));
 
-// This function goes through the array of comments and calls the render() method on each one //
-function renderAllComments(){
-  commentList.innerHTML = '';
-
-  for (var i = 0; i < belfairComments.length; i++){
-    commentList.appendChild(belfairComments[i].render());
+// ***********************************************************************
+// GETS THE DATA BACK FROM LOCAL STORAGE AND THEN RUNS THROUGH CONSTRUCTOR
+// ***********************************************************************
+if (! dataBack) {
+  console.log('no comments to render');
+} else {
+  for (var i=0; i < dataBack.length; i++){
+    new Comment(dataBack[i].userName, dataBack[i].text);
   }
+  // belfairComments = dataBack;
+  renderAllComments();
 }
+
+
+
+// for (var i=0; i < dataBack.length; i++){
+//   new Comment(dataBack[i].userName, dataBack[i].text);
+// }
+//*****************************************************************************
+
 
 // EVENT HANDLER FUNCTION FOR SUBMISSION OF COMMENTS
 function handleCommentSubmit(event){
-console.log(event.target.says.value);
 
 event.preventDefault(); //prevents page load on submit
 
@@ -49,8 +59,23 @@ var newComment = new Comment(commenter, remark);
 event.target.who.value = null;
 event.target.says.value = null;
 
-belfairComments.unshift(newComment);
+// Comment.belfairComments.unshift(newComment);
+
 renderAllComments();
+localStorage.setItem('belfairCommentsStringified', JSON.stringify(belfairComments));
+return;
+}
+
+
+//**********************************************************************************
+ // Function goes through array of comments and calls the render() method on each one //
+
+function renderAllComments(){
+  commentList.innerHTML = '';
+
+  for (var i = 0; i < belfairComments.length; i++){
+    commentList.appendChild(belfairComments[i].render());
+  }
 }
 
 //*************************************************** 
@@ -59,5 +84,4 @@ renderAllComments();
 commentForm.addEventListener('submit', handleCommentSubmit);
 
 
-localStorage.setItem('belfairCommentsStringified', JSON.stringify(belfairComments));
 
