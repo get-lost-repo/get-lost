57f973729b92fc08e4fcad036d7484486c099c46
cmorto02 <chrismorton.cm@gmail.com>
2019-01-31 11:27:53 -0800

    links are working on the front page and localstorage is working in search page

---
+++ js/splash.js
@@ -0,0 +1,38 @@
+'use strict'
+
+var categories = document.getElementById('categories');
+var dayHiking = [false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,]
+var glamping = []
+var climbing = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,]
+var beach = [false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,]
+var fishing = [false,false,false,false,false,false,false,false,false,false,false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,]
+var rv = [false,false,true,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,false,]
+
+function handleClick(event){
+  if (event.target.id === 'dayHiking'){
+    var stringifieddayHiking = JSON.stringify(dayHiking);
+    localStorage.setItem('checkedKey', stringifieddayHiking)
+  }
+  if (event.target.id === 'glamping'){
+    var stringifiedglamping = JSON.stringify(glamping);
+    localStorage.setItem('checkedKey', stringifiedglamping)
+  }
+  if (event.target.id === 'climbing'){
+    var stringifiedclimbing = JSON.stringify(climbing);
+    localStorage.setItem('checkedKey', stringifiedclimbing)
+  }
+  if (event.target.id === 'beach'){
+    var stringifiedbeach = JSON.stringify(beach);
+    localStorage.setItem('checkedKey', stringifiedbeach)
+  }
+  if (event.target.id === 'fishing'){
+    var stringifiedfishing = JSON.stringify(fishing);
+    localStorage.setItem('checkedKey', stringifiedfishing)
+  }
+  if (event.target.id === 'rvs'){
+    var stringifiedrv = JSON.stringify(rv);
+    localStorage.setItem('checkedKey', stringifiedrv)
+  }
+}
+
+categories.addEventListener('click', handleClick);
